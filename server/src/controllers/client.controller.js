6e695de1121e18b6725a9b47be0894cd9e1f03ba
A9RYA6N <aryanburnwal8@gmail.com>
2025-07-15 20:19:26 +0530

    Added backend reasoning functionality to return reasons for top result

--- server/src/controllers/client.controller.js
+++ server/src/controllers/client.controller.js
@@ -8,35 +8,44 @@ const __dirname = path.dirname(__filename);
 const match=async(req, res)=>{
     const {location, budget, skills, style}=req.body
     try {
-        const map={}
+        const map={};
         const filePath = path.resolve(__dirname, '../data/TalentProfiles.json');
         const data = await readFile(filePath, 'utf-8');
         const parsedData=JSON.parse(data)
         for(let i=0; i<parsedData.length; i++)
         {
-            map[i]=0;
+            map[i]=[0,{
+                'c':0,
+                's':0,
+                't':0,
+                'b':0
+            }];
         }
         for(let i=0; i<parsedData.length; i++)
         {
             if(parsedData[i]['city']==location)
             {
-                map[i]+=3;
+                map[i][0]+=3;
+                map[i][1]['c']+=1;
             }
             skills.forEach(skill=>{
                 if(parsedData[i]['skills'].includes(skill))
                 {
-                    map[i]+=2;
+                    map[i][0]+=2;
+                    map[i][1]['s']+=1;
                 }
             })
             style.forEach(s=>{
                 if(parsedData[i]['style_tags'].includes(s))
                 {
-                    map[i]+=1;
+                    map[i][0]+=1;
+                    map[i][1]['t']+=1;
                 }
                 parsedData[i].portfolio.forEach(project=>{
                     if(project.tags.includes(s)) 
                     {
-                        map[i]+=1;
+                        map[i][0]+=1;
+                        map[i][1]['t']+=1;
                     }
                 })
             })
@@ -46,17 +55,19 @@ const match=async(req, res)=>{
             const ul=upperLimit.replace('\u20b9','')
             if(budget>=parseInt(ll) && budget<=parseInt(ul))
             {
-                map[i]+=2
+                map[i][0]+=2;
+                map[i][1]['b']+=1;
             }
         }
         const mapArr=Object.keys(map).map((key)=>[key, map[key]])
-        mapArr.sort((a,b)=>b[1]-a[1])
+        mapArr.sort((a,b)=>b[1][0]-a[1][0])
         const resArr=[]
         for(let i=0; i<3; i++)
         {
             const talentInfo={
                 name:parsedData[mapArr[i][0]].name,
-                score:mapArr[i][1]
+                score:mapArr[i][1][0],
+                reason:mapArr[i][1][1]
             }
             resArr.push(talentInfo)
         }
